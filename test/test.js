abbce927695e0487ef4887c698a942d94bf2e1c9
Georgy Koychev <kgm.pochta@gmail.com>
2014-11-05 17:59:01 +0200

    return AMD config

--- test/test.js
+++ test/test.js
@@ -1,19 +1,8 @@
-var assert = require("assert");
-
-describe('Array', function(){
-  describe('#indexOf()', function(){
-    it('should return -1 when the value is not present', function(){
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
-    })
-  })
+var module = require('../lib');
+module.bowerPaths(function(){
+    console.log(arguments);
 });
 
-describe('MyTests', function(){
-    describe('test', function(){
-
-        var module = require('../lib');
-        console.log(module());
-
-    })
+module(function(){
+    console.log(arguments);
 });
